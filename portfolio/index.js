ee39b8b5c01470538aba4251afdda19f23b3d285
sirkevich <sirkewich92@gmail.com>
2022-01-31 23:47:56 +0200

    feat add changing images in the portfolio section

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,3 +1,91 @@
+const i18Obj = {
+  'en': {
+    'skills': 'Skills',
+    'portfolio': 'Portfolio',
+    'video': 'Video',
+    'price': 'Price',
+    'contacts': 'Contacts',
+    'hero-title': 'Alexa Rise',
+    'hero-text': 'Save sincere emotions, romantic feelings and happy moments of life together with professional photographer Alexa Rise',
+    'hire': 'Hire me',
+    'skill-title-1': 'Digital photography',
+    'skill-text-1': 'High-quality photos in the studio and on the nature',
+    'skill-title-2': 'Video shooting',
+    'skill-text-2': 'Capture your moments so that they always stay with you',
+    'skill-title-3': 'Rotouch',
+    'skill-text-3': 'I strive to make photography surpass reality',
+    'skill-title-4': 'Audio',
+    'skill-text-4': 'Professional sounds recording for video, advertising, portfolio',
+    'winter': 'Winter',
+    'spring': 'Spring',
+    'summer': 'Summer',
+    'autumn': 'Autumn',
+    'price-description-1-span-1': 'One location',
+    'price-description-1-span-2': '120 photos in color',
+    'price-description-1-span-3': '12 photos in retouch',
+    'price-description-1-span-4': 'Readiness 2-3 weeks',
+    'price-description-1-span-5': 'Make up, visage',
+    'price-description-2-span-1': 'One or two locations',
+    'price-description-2-span-2': '200 photos in color',
+    'price-description-2-span-3': '20 photos in retouch',
+    'price-description-2-span-4': 'Readiness 1-2 weeks',
+    'price-description-2-span-5': 'Make up, visage',
+    'price-description-3-span-1': 'Three locations or more',
+    'price-description-3-span-2': '300 photos in color',
+    'price-description-3-span-3': '50 photos in retouch',
+    'price-description-3-span-4': 'Readiness 1 week',
+    'price-description-3-span-5': 'Make up, visage, hairstyle',
+    'order': 'Order shooting',
+    'contact-me': 'Contact me',
+    'send-message': 'Send message'
+  },
+  'ru': {
+    'skills': 'Навыки',
+    'portfolio': 'Портфолио',
+    'video': 'Видео',
+    'price': 'Цены',
+    'contacts': 'Контакты',
+    'hero-title': 'Алекса Райс',
+    'hero-text': 'Сохраните искренние эмоции, романтические переживания и счастливые моменты жизни вместе с профессиональным фотографом',
+    'hire': 'Пригласить',
+    'skill-title-1': 'Фотография',
+    'skill-text-1': 'Высококачественные фото в студии и на природе',
+    'skill-title-2': 'Видеосъемка',
+    'skill-text-2': 'Запечатлите лучшие моменты, чтобы они всегда оставались с вами',
+    'skill-title-3': 'Ретушь',
+    'skill-text-3': 'Я стремлюсь к тому, чтобы фотография превосходила реальность',
+    'skill-title-4': 'Звук',
+    'skill-text-4': 'Профессиональная запись звука для видео, рекламы, портфолио',
+    'winter': 'Зима',
+    'spring': 'Весна',
+    'summer': 'Лето',
+    'autumn': 'Осень',
+    'price-description-1-span-1': 'Одна локация',
+    'price-description-1-span-2': '120 цветных фото',
+    'price-description-1-span-3': '12 отретушированных фото',
+    'price-description-1-span-4': 'Готовность через 2-3 недели',
+    'price-description-1-span-5': 'Макияж, визаж',
+    'price-description-2-span-1': 'Одна-две локации',
+    'price-description-2-span-2': '200 цветных фото',
+    'price-description-2-span-3': '20 отретушированных фото',
+    'price-description-2-span-4': 'Готовность через 1-2 недели',
+    'price-description-2-span-5': 'Макияж, визаж',
+    'price-description-3-span-1': 'Три локации и больше',
+    'price-description-3-span-2': '300 цветных фото',
+    'price-description-3-span-3': '50 отретушированных фото',
+    'price-description-3-span-4': 'Готовность через 1 неделю',
+    'price-description-3-span-5': 'Макияж, визаж, прическа',
+    'order': 'Заказать съемку',
+    'contact-me': 'Свяжитесь со мной',
+    'send-message': 'Отправить'
+  }
+}
+// export default i18Obj;
+
+
+
+
+
 const hamburger = document.querySelector('.hamburger');
 const navListBurgerOpen = document.querySelector('.nav-list');
 const links = document.querySelector('.nav-list');
@@ -15,4 +103,143 @@ function closeMenu() {
 hamburger.addEventListener('click', toggleMenu);
 links.addEventListener('click',closeMenu);
 
-console.log('блок header -- 6 балов \nблок hero -- 6 балов \nблок skills -- 6 балов \nблок portfolio -- 6 балов \nблок video -- 6 балов \nблок price -- 6 балов \nблок contacts -- 6 балов \nблок footer -- 6 балов \n\nнет полосы прокрутки при ширине страницы от 1440рх до 768рх -- 5 балов \nнет полосы прокрутки при ширине страницы от 768рх до 480рх -- 5 балов \nнет полосы прокрутки при ширине страницы от 480рх до 320рх -- 5 балов \n\nпри ширине страницы 768рх панель навигации скрывается, появляется бургер-иконка -- 2 бала \nпри нажатии на бургер-иконку справа плавно появляется адаптивное меню, бургер-иконка изменяется на крестик -- 4 бала \nпвысота адаптивного меню занимает всю высоту экрана. При ширине экрана 768-620рх вёрстка меню соответствует макету, когда экран становится уже, меню занимает всю ширину экрана -- 4 бала \nпри нажатии на крестик адаптивное меню плавно скрывается уезжая за правую часть экрана, крестик превращается в бургер-иконку -- 4 бала \nпри нажатии на крестик адаптивное меню плавно скрывается уезжая за правую часть экрана, крестик превращается в бургер-иконку -- 4 бала \nбургер-иконка, которая при клике превращается в крестик, создана при помощи css-анимаций без использования изображений -- 2 бала \nссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям -- 2 бала \nпри клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, крестик превращается в бургер-иконку -- 4бала');
+// console.log('блок header -- 6 балов \nблок hero -- 6 балов \nблок skills -- 6 балов \nблок portfolio -- 6 балов \nблок video -- 6 балов \nблок price -- 6 балов \nблок contacts -- 6 балов \nблок footer -- 6 балов \n\nнет полосы прокрутки при ширине страницы от 1440рх до 768рх -- 5 балов \nнет полосы прокрутки при ширине страницы от 768рх до 480рх -- 5 балов \nнет полосы прокрутки при ширине страницы от 480рх до 320рх -- 5 балов \n\nпри ширине страницы 768рх панель навигации скрывается, появляется бургер-иконка -- 2 бала \nпри нажатии на бургер-иконку справа плавно появляется адаптивное меню, бургер-иконка изменяется на крестик -- 4 бала \nпвысота адаптивного меню занимает всю высоту экрана. При ширине экрана 768-620рх вёрстка меню соответствует макету, когда экран становится уже, меню занимает всю ширину экрана -- 4 бала \nпри нажатии на крестик адаптивное меню плавно скрывается уезжая за правую часть экрана, крестик превращается в бургер-иконку -- 4 бала \nпри нажатии на крестик адаптивное меню плавно скрывается уезжая за правую часть экрана, крестик превращается в бургер-иконку -- 4 бала \nбургер-иконка, которая при клике превращается в крестик, создана при помощи css-анимаций без использования изображений -- 2 бала \nссылки в адаптивном меню работают, обеспечивая плавную прокрутку по якорям -- 2 бала \nпри клике по ссылке в адаптивном меню адаптивное меню плавно скрывается, крестик превращается в бургер-иконку -- 4бала');
+
+// ----------------------------------adding js----------------------------
+
+
+
+
+
+
+// -----------------------change images-----------------------
+
+
+const portfolioImages = document.querySelectorAll('.portfolio-img');
+
+const portfolioBtns = document.querySelector('.portfolio-buttons-container');
+
+
+function changeImage(event) {
+  if(event.target.classList.contains('portfolio-btn')) {
+    { 
+       if (event.target.classList.contains('winter')){
+        portfolioImages.forEach((img, index) => img.src = `./assets/img/Winter/${index + 1}.jpg`);
+    } else if (event.target.classList.contains('spring')){
+      portfolioImages.forEach((img, index) => img.src = `./assets/img/Spring/${index + 1}.jpg`);
+    } else if (event.target.classList.contains('summer')){
+      portfolioImages.forEach((img, index) => img.src = `./assets/img/Summer/${index + 1}.jpg`);
+    } else if (event.target.classList.contains('autumn')){
+      portfolioImages.forEach((img, index) => img.src = `./assets/img/Autumn/${index + 1}.jpg`);
+    }
+      
+    }
+  }
+}
+portfolioBtns.addEventListener("click", changeImage);
+  
+
+
+// // -----------------------active buttons -----------------------
+
+
+
+
+document.querySelectorAll('.portfolio-btn').forEach(element => {
+  element.addEventListener('click', item => {
+    document.querySelectorAll('.portfolio-btn').forEach(element => element.classList.remove('portfolio-btn-active'));
+    item.target.classList.add('portfolio-btn-active');
+  })
+})
+
+
+
+
+  // var allBtns = document.querySelectorAll('button');
+
+  //   allBtns.forEach(item => {
+
+  //     // if (item.addEventListener('click')) {
+  //     //   if (item.classList.contains('portfolio-btn')) {
+  //     //       item.classList.remove('portfolio-btn-active');
+  //     //     }
+  //     // }
+
+  //     // function addActiveProp () {
+  //     // //  if (item.classList.contains('portfolio-btn')) {
+  //     // //   item.classList.remove('portfolio-btn-active');
+  //     // // }
+  //     // console.log(item)
+  //     // }
+  
+  //     // if (item.classList.contains('portfolio-btn')) {
+  //     //   item.classList.remove('portfolio-btn-active');
+  //     // }
+
+  //     if (item.classList.contains('winter')) {
+  //       item.addEventListener('click', addActiveProp);
+  //     }
+  // });
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------translate page---------------------
+
+
+// function getTranslate() {
+//   const dataAtr = document.querySelectorAll('data-i18');
+
+//   dataAtr.forEach(element => {
+    
+//   });
+
+// }
+
+
+
+
+// --------------------change theme------------------- 
+
+let arr = ['skills', 'portfolio', 'video', 'price'];
+
+let changeThemeBtn = document.querySelector('.theme-btn');
+
+changeThemeBtn.addEventListener('click', changeTheme);
+
+function changeTheme () {
+  
+  arr.forEach(element => {
+    let item = document.querySelector(`.${element}`);
+    if (item.classList.contains('light-theme')) {
+     return item.classList.toggle('light-theme'), 
+     document.documentElement.style.setProperty('--body-color', '#000'),
+     document.documentElement.style.setProperty('--section-title-color', '#bdae82'),
+     document.documentElement.style.setProperty('--section-after', '#bdae82'),
+     document.documentElement.style.setProperty('--btn-text-color', '#bdae82'),
+     document.documentElement.style.setProperty('--btn-text-color-active', '#000'),
+     document.documentElement.style.setProperty('--color-burger', '#000'),
+     document.documentElement.style.setProperty('--link-color', '#fff'),
+     document.documentElement.style.setProperty('--hamburger-color', '#fff');
+      
+    } else {
+     return item.classList.add('light-theme'), 
+     document.documentElement.style.setProperty('--body-color', '#fff'),
+     document.documentElement.style.setProperty('--section-title-color', '#000'),
+     document.documentElement.style.setProperty('--section-after', '#000'),
+     document.documentElement.style.setProperty('--btn-text-color', '#000'),
+     document.documentElement.style.setProperty('--btn-text-color-active', '#fff'),
+     document.documentElement.style.setProperty('--color-burger', '#fff'),
+     document.documentElement.style.setProperty('--link-color', '#000'),
+     document.documentElement.style.setProperty('--hamburger-color', '#000');
+    }
+  });
+}
